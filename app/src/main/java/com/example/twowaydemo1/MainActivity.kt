44867c8166ab0b00d62c4c8cc722e632d6cbf494
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-03 03:49:13 +0400

    add binding

--- app/src/main/java/com/example/twowaydemo1/MainActivity.kt
+++ app/src/main/java/com/example/twowaydemo1/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.twowaydemo1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.twowaydemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
